667634c6cc975fae375fdba8735d38563755e061
skanda108 <sbthai94@gmail.com>
2023-08-07 11:20:07 -0500

    added host to seed.ts

--- server/db/seed.ts
+++ server/db/seed.ts
@@ -4,7 +4,7 @@ const { db, Users, Places, Events, Reels, Rsvp, Likes, Notifications, Friendship
 db.options.logging = false;
 
 const seedSqlize = () => {
-  mysql.createConnection({ user: 'root', password: '', })
+  mysql.createConnection({ user: 'root', password: '', host: '127.0.0.1' })
     .then((db) => db.query('CREATE DATABASE IF NOT EXISTS `theSpot`').then(() => db.end()))
     .then(() => console.log('\x1b[33m', '\nDatabase (MySQL): \'theSpot\' successfully created!'))
     .then(() => Users.sync({ force: true }))
