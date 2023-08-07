18d76bcc969138abccf38053e38bc510f25b97a4
wisdomjackie <wisdomjackie@gmail.com>
2023-08-07 11:25:51 -0500

    add host to sequelize connection

--- server/db/seed.ts
+++ server/db/seed.ts
@@ -4,7 +4,7 @@ const { db, Users } = require('./index.ts');
 db.options.logging = false;
 
 const seedSqlize = () => {
-  mysql.createConnection({ user: 'root', password: '', })
+  mysql.createConnection({ user: 'root', password: '', host: '127.0.0.1'})
     .then((db) => db.query('CREATE DATABASE IF NOT EXISTS `theSpot`').then(() => db.end()))
     .then(() => console.log('\x1b[33m', '\nDatabase (MySQL): \'theSpot\' successfully created!'))
     .then(() => Users.sync({ force: true }))
