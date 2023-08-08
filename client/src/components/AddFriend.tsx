39ff5e38b44d84721ee656a289411a29460b5914
janvierjr <bernie@myheartsleeve.com>
2023-08-08 00:48:26 -0500

    create addFriend component with mui

--- client/src/components/AddFriend.tsx
+++ client/src/components/AddFriend.tsx
@@ -1,7 +1,60 @@
 import * as React from 'react';
+import Box from '@mui/material/Box';
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import fakeUsers from '../../../server/db/fakeUserData.json';
+import Avatar from '@mui/material/Avatar';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#f0f465',
+      dark: '#f433ab',
+      contrastText: '#0b0113',
+    },
+    secondary: {
+      main: '#f433ab',
+      dark: '#f0f465',
+      contrastText: '#0b0113',
+    },
+  },
+});
+
+type User = {
+  id: number;
+  username: string;
+  displayName: string;
+  type: string;
+  geolocation: string;
+  mapIcon: string;
+  birthday: string;
+  privacy: string;
+  accessibility: string;
+};
 
 const AddFriend = () => {
-  return <h1>AddFriend</h1>;
+  return (
+    <ThemeProvider theme={theme}>
+      <div className='friend-request'>
+        <Box className="friend-box">
+          <Fab
+            size='small'
+            color='primary'
+            aria-label='add'
+            className='friend-add-btn'
+          >
+            <AddIcon />
+          </Fab>
+          <Avatar
+            className='friend-avatar'
+            sx={{ width: 48, height: 48 }}
+            alt='user_id name'
+          />
+        </Box>
+      </div>
+    </ThemeProvider>
+  );
 };
 
 export default AddFriend;
