938327ca6054b9898106e04efa4c53a65b743502
Benjamin Klein <benjaminklein99@gmail.com>
2023-08-08 15:53:49 -0500

    switch branch to map

--- client/src/components/Map/Map.tsx
+++ client/src/components/Map/Map.tsx
@@ -1,48 +1,21 @@
 import * as React from 'react';
+import GoogleMapReact from 'google-map-react';
+
 
 const Map = () => {
-  // // Initialize and add the map
-  // let map;
-  // async function initMap(): Promise<void> {
-  //   // The location of Uluru
-  //   const position = { lat: -25.344, lng: 131.031 };
-
-  //   // Request needed libraries.
-  //   //@ts-ignore
-  //   const { Map } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
-  //   const { AdvancedMarkerElement } = await google.maps.importLibrary("marker") as google.maps.MarkerLibrary;
-
-  //   // The map, centered at Uluru
-  //   map = new Map(
-  //     document.getElementById('map') as HTMLElement,
-  //     {
-  //       zoom: 4,
-  //       center: position,
-  //       mapId: 'DEMO_MAP_ID',
-  //     }
-  //   );
-
-  //   // The marker, positioned at Uluru
-  //   const marker = new AdvancedMarkerElement({
-  //     map: map,
-  //     position: position,
-  //     title: 'Uluru'
-  //   });
-  // }
-
-  // initMap();
-
   return (
     <div>
       <h1>Map</h1>
-      <div id="map"></div>
-      <iframe
+      <div>
+        <GoogleMapReact defaultZoom={10} defaultCenter={{lat: 34.0522, lng: -118.2437}}></GoogleMapReact>
+      </div>
+      {/* <iframe
         width="600"
         height="450"
         style={{border: 0}}
         loading="lazy"
         src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAYtb7y6JZ2DxgdIESWJky8NyhWuu_YFVg&q=Tipitinas,New+Orleans+LA">
-        </iframe>
+        </iframe> */}
     </div>
 
   );
