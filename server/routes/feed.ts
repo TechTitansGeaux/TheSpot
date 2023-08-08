5c0f698b5a845e5cb86cd71a568b0a72eba224cc
skanda108 <sbthai94@gmail.com>
2023-08-08 12:31:11 -0500

    feed route

---
+++ server/routes/feed.ts
@@ -0,0 +1,29 @@
+const express = require('express');
+const feedRouter = express.Router();
+const { Reels, Users, Events } = require('../db/index');
+// import Reels from '../db/index';
+
+feedRouter.get('/reel', (req: any, res: any) => {
+  // will be changed to find by filter
+  Reels.findAll()
+    .then((response: any) => {
+      if (response === null) {
+        console.log('feed does not exist');
+        res.sendStatus(404);
+      } else {
+        res.status(200).send(response);
+      }
+    })
+    .then((response: any) =>{
+      const { user_id } = req.body;
+      console.log(user_id);
+    })
+    .catch((err: any) => {
+      console.error('Cannot GET feed:', err);
+      res.sendStatus(500);
+    })
+});
+
+
+export default feedRouter;
+
