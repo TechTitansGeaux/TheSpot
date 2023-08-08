b7cdceba1cbfc0422067c8d87352c04bf9ce8514
janvierjr <bernie@myheartsleeve.com>
2023-08-08 00:47:55 -0500

    update mui navigation start

--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -1,21 +1,46 @@
 import * as React from 'react';
 import { Outlet, NavLink } from 'react-router-dom';
+import AppBar from '@mui/material/AppBar';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Toolbar } from '@mui/material';
+import Avatar from '@mui/material/Avatar';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#f0f465',
+      dark: '#f433ab',
+      contrastText: '#0b0113',
+    },
+    secondary: {
+      main: '#f433ab',
+      dark: '#f0f465',
+      contrastText: '#0b0113',
+    },
+  },
+});
 
 const Navigation = () => {
   return (
     <>
       <nav>
-        <ul>
-          <li>
-            <NavLink to='/Feed'>Feed</NavLink>
-          </li>
-          <li>
-            <NavLink to='/Map'>Map</NavLink>
-          </li>
-          <li>
-            <NavLink to='/WebcamDisplay'>WebcamDisplay</NavLink>
-          </li>
-        </ul>
+      <ThemeProvider theme={theme}>
+          <AppBar position='fixed'>
+            <Toolbar>
+              <ul>
+                <li>
+                  <NavLink to='/Feed'>Feed</NavLink>
+                </li>
+                <li>
+                  <NavLink to='/Map'>Map</NavLink>
+                </li>
+                <li>
+                  <NavLink to='/WebcamDisplay'>WebcamDisplay</NavLink>
+                </li>
+              </ul>
+            </Toolbar>
+          </AppBar>
+          </ThemeProvider>
       </nav>
       <Outlet />
     </>
