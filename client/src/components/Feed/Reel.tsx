7cbb59abc971d5e991b522948e1c5b8e93a953b7
janvierjr <bernie@myheartsleeve.com>
2023-08-08 22:54:02 -0500

    update types for PROPS AND update routes

--- client/src/components/Feed/Reel.tsx
+++ client/src/components/Feed/Reel.tsx
@@ -5,31 +5,32 @@ import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
-import AddFriend from '../AddFriend'
+import AddFriend from '../AddFriend';
 
 // jackie added public_id and url instead of video
-type Reel = {
-  id: number;
-  public_id: number;
-  url: string,
-  text: string;
-  like_count: number;
-  UserId: number;
-  EventId: number;
+type Props = {
+  reels:
+  {
+    id?: string;
+    public_id?: number;
+    url?: string;
+    text?: string;
+    like_count?: number;
+    UserId?: number;
+    EventId?: number;
+  }[]
 };
 
-
-
-const Reel = () => {
+const Reel: React.FC<Props> = ({reels}) => {
   return (
     <div className='reel-container'>
-      {fakeReels?.map((reel: Reel) => {
+      {reels?.map((reel) => {
         return (
           <div key={reel.id + 'reel'}>
             {/* jackie replaced reel.video here with reel.url */}
             <div className='video' id={reel.url}>
               <p className='video-text'>{reel.text}</p>
-              <AddFriend/>
+              <AddFriend />
             </div>
             <div className='video-links-container'>
               <Box sx={{ maxWidth: 400 }}>
