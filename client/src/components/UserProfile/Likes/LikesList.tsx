0b6b7c4d03b77cf813d2b8c4bc478e19a48c3a4d
skanda108 <sbthai94@gmail.com>
2023-08-19 18:04:38 -0500

    likes list entry functional

--- client/src/components/UserProfile/Likes/LikesList.tsx
+++ client/src/components/UserProfile/Likes/LikesList.tsx
@@ -1,5 +1,22 @@
 import * as React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import LikesEntry from './LikesEntry';
+
+type User = {
+  id: number;
+  username: string;
+  displayName: string;
+  type: string;
+  geolocation: string;
+  mapIcon: string;
+  birthday: string;
+  privacy: string;
+  accessibility: string;
+  email: string;
+  picture: string;
+  googleId: string;
+};
 
 type Props = {
   user: {
@@ -16,13 +33,72 @@ type Props = {
     picture: string;
     googleId: string;
   };
+  allUsers: [User];
 };
-const LikesList: React.FC<Props> = ({user}) => {
+const LikesList: React.FC<Props> = ({user, allUsers}) => {
+  const [likesArr, setLikesArr] = useState([]); // user's own reels that have been liked FROM likes table
+  const [userReels, setUserReels] = useState([]); // user's own reels
+
+    // get your own reels
+    const getOwnReels = () => {
+      axios
+        .get('/feed/reel/user')
+        .then((response: any) => {
+          //console.log('users own reels:', response.data);
+          setUserReels(response.data);
+        })
+        .catch((err: any) => {
+          console.error('Cannot get own reels:', err);
+        })
+    };
+
+    useEffect(() => {
+      getOwnReels();
+    }, [user]);
+
+    // get reels that have been liked AND checked
+    const getLikes = () => {
+      const likes: any = []; // user's reels that have been liked
+      if (user) {
+        axios
+          .get('/likes/likes')
+          .then((response) => {
+            //console.log('likes:', response.data);
+            for (let i = 0; i < response.data.length; i++) {
+              for (let j = 0; j < userReels.length; j++) {
+                if (response.data[i].ReelId === userReels[j].id) {
+                  likes.push(response.data[i]); }
+              }
+            }
+            setLikesArr(likes);
+            console.log('likes array:', likesArr);
+            console.log('user reels:', userReels);
+          })
+          .catch((err) => {
+            console.error('Could not GET all likes:', err);
+          });
+      }
+    };
+
+    useEffect(() => {
+      getLikes();
+    }, [user]);
+
   return (
+    <ul>
     <div className='container-full-w'>
       <h1>LikesList</h1>
-      <LikesEntry />
+      {likesArr.map((like, index) => {
+        return <LikesEntry key={`${index}-${like.id}`}
+        like={like}
+        user={user}
+        allUsers={allUsers}
+        />
+      })}
+
     </div>
+
+    </ul>
   );
 };
 
